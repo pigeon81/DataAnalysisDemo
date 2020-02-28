88748cef5876b0318f6d81e76b4ab305ff1b1301
pigeon81 <pigeon81>
2020-02-29 02:23:28 +0530

    first push

---
+++ sample.py
@@ -0,0 +1,42 @@
+
+#!/usr/bin/env python3
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("/home/pigeon/Documents/UnderstandingPython/gapminder-FiveYearData.csv")
+
+print(df)
+
+print(df.head())
+
+print(df.shape)
+
+print(df.columns)
+
+print(df.dtypes)
+
+print(df.info())
+
+country_df = df['country']
+
+print(country_df.head)
+
+print(df.head(n=15))
+
+print(df.head())
+
+print(df.shape)
+
+# print(df.columns)
+
+# print(df.dtypes)
+
+# print(df.info())
+
+country_df = df['country']
+
+print(country_df.head)
+
+print(df.head(n=15))
